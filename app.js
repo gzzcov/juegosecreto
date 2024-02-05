8d48faac645cf3145fe878bcfc84366b55ee6a15
gzzcov <81454603+gzzcov@users.noreply.github.com>
2024-02-04 21:14:25 -0600

    the final countdown

---
+++ app.js
@@ -0,0 +1,11 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento()
+{
+    let titulo = document.querySelector('h1');
+    titulo.innerHTML = 'Juego deel número secreto';
+}
+
